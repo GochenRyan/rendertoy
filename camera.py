59833013f79e844149757a79c68204cf2d7e2bdb
GochenRyan <rangaocheng@outlook.com>
2021-01-15 21:12:00 +0800

    实现UVN相机模型代码

--- camera.py
+++ camera.py
@@ -2,29 +2,45 @@ import numpy as np
 import rtmath
 
 from geometry import vector
-
+"""
+相机空间：右手系
+"""
 class CUVNCamera(object):
 	def __init__(self, vEye=None, vAt=None, vUp=None):
-		self.m_vEye = vector([0, 0, -3, 1]) if vEye is None else vEye
-		self.m_vAt = vector([0.23, 0, 0, 1]) if vAt is None else vAt
-		self.m_vUp = vector([0, 1, 0, 1]) if vUp is None else vUp
+		self.m_vEye = vector([0, 0, -3, 1]) if vEye is None else vEye  # 相机的位置
+		self.m_vAt = vector([0.23, 0, 0, 1]) if vAt is None else vAt  # 注视目标的位置
+		self.m_vUp = vector([0, 1, 0, 1]) if vUp is None else vUp  # 上向量
 		self.m_mViewTrans = None
 
 	def GetViewTrans(self):
 		if not self.m_mViewTrans:
-			pass
-		fN = rtmath.normalize(self.m_vAt - self.m_vEye)[:3]
-		fV = rtmath.normalize(self.m_vUp)[:3]
-		fU = None # 右手系(todo:确定正负)
-
+			self.calViewTrans()
 
 		return self.m_mViewTrans
 
+	def calViewTrans(self):
+		# 观察空间为右手系，这里算出U、N、V轴在世界空间的表示
+		fN = rtmath.normalize(self.m_vEye - self.m_vAt)[:3]
+		fU = -rtmath.normalize(np.cross(self.m_vUp[:3], fN))
+		fV = rtmath.normalize(np.cross(fU, fN))
+
+		mTemp = np.eye(4)
+		mTemp[0, :3] = fU
+		mTemp[1, :3] = fV
+		mTemp[2, :3] = fN
+		mTemp[3, 0] = -np.dot(fU, self.m_vEye[:3])
+		mTemp[3, 1] = -np.dot(fV, self.m_vEye[:3])
+		mTemp[3, 2] = -np.dot(fN, self.m_vEye[:3])
+		self.m_mViewTrans = mTemp
+
 	def SetPos(self, vAt):
-		pass
+		self.m_vAt = vAt
+		self.calViewTrans()
 
 	def SetEye(self, vEye):
-		pass
+		self.m_vEye = vEye
+		self.calViewTrans()
 
 	def SetUp(self, vUp):
-		pass
+		self.m_vUp = vUp
+		self.calViewTrans()
